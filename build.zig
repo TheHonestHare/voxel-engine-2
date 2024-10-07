d122d52314c560cee5c1d58bc4bd24857386cd55
Lim2202538 <2202538@spsd.sk.ca>
2024-10-07 10:50:03 -0600

    fetch zbgfx with build system

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -31,9 +30,9 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("zbgfx", zbgfx.module("zbgfx"));
     exe.linkLibrary(zbgfx.artifact("bgfx"));
     b.installArtifact(zbgfx.artifact("shaderc"));
-    
+
     b.installArtifact(exe);
-    
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run the app");
